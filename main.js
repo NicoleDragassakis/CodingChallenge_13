3405ce61b6021edb595de7b4c4e4633a7225bcbe
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-11-03 17:03:39 -0500

    Handle errors with .catch() and display error message. Added css.

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
 
 //TASK TWO
 
-const productContainer = document.getElementById('product-container');//getting element from ID
-const errorMessage = document.getElementById('error-message'); //getting element from ID
+const productContainer = document.getElementById('product-container'); //gets the element from the DOM 
 
-fetch('https://www.course-api.com/javascript-store-products')
+fetch('https://www.course-api.com/javascript-store-products') //fetching the data from the API
     .then(response => {
         if (!response.ok) {
-            throw new Error('Network response was not ok'); 
+            throw new Error('Network response was not ok'); //checks in respose is ok if not then throws an error so it can be detected
         }
         return response.json();
     })
     .then(data => {
-        displayProducts(data);
+        displayProducts(data); //calls display Products
     })
+    //TASK FOUR
     .catch(error => {
-        errorMessage.textContent = "Error Loading Contents";
-        // errorMessage.style.display = "block";
-        console.error('Fetch error:', error);
+        console.error('Ftech operation Error:', error); //logs an error if there was a problem w the fetch operation
+        productContainer.innerHTML = `<p>Failed to load contents</p>`; //displays message to user if there is an error
     });
 
  //TASK THREE
-
+//function displays products dynamiclly
  function displayProducts(data) {
-    data.forEach(product => {
-        const productDiv = document.createElement('div');
+    data.forEach(item => { //a loop
+        const product = item.fields; //access the produt data
+        const productDiv = document.createElement('div'); //new div to hold the produt data
         productDiv.classList.add('product');
-        productDiv.innerHTML =`
+        //formats html 
+        productDiv.innerHTML = `
             <h2>${product.name}</h2>
             <img src="${product.image}" alt="${product.name}">
             <p>Price: $${product.price}</p>
             <p>Company: ${product.company}</p>
         `;
+
         productContainer.appendChild(productDiv);
     });
- }
+}
+
+
